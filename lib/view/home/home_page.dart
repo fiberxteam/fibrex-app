4cc93249ddd948d5b93c3144c34cfb683db46717
AliJassib <OscarAli.info@gmail.com>
2024-05-14 06:36:14 +0300

    Update localization strings and add new service cards in home_page.dart

--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -3,7 +3,9 @@ import 'package:fiber/view/home/components/custom_app_bar.dart';
 import 'package:fiber/view/home/components/custom_offers_card.dart';
 import 'package:fiber/view/home/components/custom_service_card.dart';
 import 'package:fiber/view/home/components/custom_sub_scribe_widget.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,7 +24,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           SizedBox(height: Insets.exLarge * 1.2),
           const CustomAppBar(),
-          SizedBox(height: Insets.medium),
+          SizedBox(height: Insets.small),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: Insets.small),
             child: const CustomSubScribeWidget(),
@@ -103,10 +105,80 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          SizedBox(height: Insets.medium),
+          SizedBox(height: Insets.small),
           CustomOffersCard(images: images),
-          SizedBox(height: Insets.exLarge * 5),
-        ].animate(interval: 50.ms).fadeIn(),
+          SizedBox(height: Insets.medium),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: Insets.small),
+            child: Text(
+              'آخر الأخبار'.tr,
+              style: context.theme.textTheme.titleSmall!.copyWith(
+                color: context.theme.colorScheme.scrim,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: Insets.small),
+          AspectRatio(
+            aspectRatio: 12 / 9,
+            child: ListView.separated(
+              padding: EdgeInsets.symmetric(horizontal: Insets.small),
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              itemCount: images.length,
+              separatorBuilder: (context, index) =>
+                  SizedBox(width: Insets.small),
+              itemBuilder: (context, index) {
+                return Container(
+                  width: context.width * 0.6,
+                  padding: EdgeInsets.all(Insets.small),
+                  decoration: BoxDecoration(
+                    color: context.theme.colorScheme.surface,
+                    borderRadius: BorderRadius.circular(Insets.medium),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 13 / 9,
+                        child: Container(
+                          clipBehavior: Clip.antiAlias,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(Insets.small),
+                          ),
+                          child: Image.asset(
+                            images[index],
+                            width: context.width * 0.6,
+                            height: context.height * 0.3,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: Insets.small),
+                      Text(
+                        'آخر تحديثات الكيمنك!',
+                        style: context.theme.textTheme.titleSmall!.copyWith(
+                          color: context.theme.colorScheme.primary,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: Insets.exSmall),
+                      Text(
+                        'باقات فايبر أكس تقدم مجموعة خدمات متنوعة من الباقات المُصممة لتلبية احتياجات العملاء ، موفرة إنترنت عالي السرعة مع ميزات وفوائد مختلفة.',
+                        style: context.theme.textTheme.labelSmall!.copyWith(
+                          color: context.theme.colorScheme.outline,
+                        ),
+                        maxLines: 4,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+          SizedBox(height: Insets.exLarge * 2.5),
+        ].animate(interval: 30.ms).fadeIn(),
       ),
     );
   }
