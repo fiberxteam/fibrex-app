d706623e5d38bd15dc5e20236c2f3bf1a380da12
AliJassib <OscarAli.info@gmail.com>
2024-05-14 06:39:14 +0300

    Add CustomNewsCard component to home_page.dart

---
+++ lib/view/home/components/custom_news_card.dart
@@ -0,0 +1,71 @@
+import 'package:fiber/config/constant.dart';
+
+class CustomNewsCard extends StatelessWidget {
+  const CustomNewsCard({
+    super.key,
+    required this.images,
+  });
+
+  final List<String> images;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 12 / 9,
+      child: ListView.separated(
+        padding: EdgeInsets.symmetric(horizontal: Insets.small),
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: images.length,
+        separatorBuilder: (context, index) => SizedBox(width: Insets.small),
+        itemBuilder: (context, index) {
+          return Container(
+            width: context.width * 0.6,
+            padding: EdgeInsets.all(Insets.small),
+            decoration: BoxDecoration(
+              color: context.theme.colorScheme.surface,
+              borderRadius: BorderRadius.circular(Insets.medium),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AspectRatio(
+                  aspectRatio: 13 / 9,
+                  child: Container(
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Insets.small),
+                    ),
+                    child: Image.asset(
+                      images[index],
+                      width: context.width * 0.6,
+                      height: context.height * 0.3,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(height: Insets.small),
+                Text(
+                  'آخر تحديثات الكيمنك!',
+                  style: context.theme.textTheme.titleSmall!.copyWith(
+                    color: context.theme.colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: Insets.exSmall),
+                Text(
+                  'باقات فايبر أكس تقدم مجموعة خدمات متنوعة من الباقات المُصممة لتلبية احتياجات العملاء ، موفرة إنترنت عالي السرعة مع ميزات وفوائد مختلفة.',
+                  style: context.theme.textTheme.labelSmall!.copyWith(
+                    color: context.theme.colorScheme.outline,
+                  ),
+                  maxLines: 4,
+                  overflow: TextOverflow.ellipsis,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
